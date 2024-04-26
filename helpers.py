1686c20b3315f2f0b762dc0cbd86ae8190dd5869
OkarFabianTheWise <108690496+OkarFabianTheWise@users.noreply.github.com>
2024-04-26 23:41:37 +0100

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,4 +1,16 @@
-import requests
+import requests, aiohttp, asyncio
+
+def fetch_pools(address):
+    try:
+        url = "https://api.raydium.io/v2/main/pairs"
+        response = requests.get(url)
+        if response.status_code == 200:
+            resp = response.json()
+            print(resp)
+        else:
+            print("failed")
+    except Exception as d:
+        print("fetch_pools err:", d)
 
 def getSymbol(token):
     # usdc and usdt
@@ -37,3 +49,28 @@ def getSymbol(token):
             return "USDC", "SOL"
         elif token == 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v':
             return "USDT", "SOL"
+        
+
+async def get_user_balance(wallet, token_address):
+    try:
+        url = f"https://public-api.birdeye.so/v1/wallet/token_balance?wallet={wallet}&token_address={token_address}"
+
+        access = {
+            "x-chain": "solana",
+            "X-API-KEY": "5438498eb1e64d51a836e9458b2f442e"
+        }
+        
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=access) as response:
+                if response.status == 200:
+                    response_object = await response.json()
+                    #print(response)
+                    if not response_object['data']:
+                        return 0
+                    elif response_object['data']:
+                        return response_object['data']['balance']
+                else:
+                    return 0
+    except Exception as d:
+        print("get_price:", d)
+        return 0
