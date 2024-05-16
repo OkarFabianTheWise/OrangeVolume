1b78bbdf7d93a2b50151045e4c959382f87634b2
OkarFabianTheWise <108690496+OkarFabianTheWise@users.noreply.github.com>
2024-05-16 11:45:40 +0100

    Update sell.py

--- sell.py
+++ sell.py
@@ -9,82 +9,13 @@ from solders.keypair import Keypair
 from create_close_account import sell_get_token_account,get_token_account, make_swap_instruction
 
 endpoint = "https://api.mainnet-beta.solana.com"
-#"https://solana-mainnet.core.chainstack.com/09bd70b85bb848a5f59c0e384bdcb948"
-url = 'https://solana-mainnet.g.alchemy.com/v2/z9C5AM0W9ltsD2HBSbmCgaByy9FSZASc'
+
 solana_client = Client(endpoint)
 print(solana_client.is_connected())
 
 import time, base58
 
-
-poolkeysflush = {'amm_id': Pubkey.from_string(
-    'DVj1swqYnqPzPvhwcGe9JvTJ46szGEVCYhrHZ6oUjia9',
-), 'authority': Pubkey.from_string(
-    '5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1',
-), 'base_mint': Pubkey.from_string(
-    'H5UVM4S9Y5kbuVCEEyJ3NZ38CM8cxUEJrm9gNhswKSdy',
-), 'base_decimals': 9, 'quote_mint': Pubkey.from_string(
-    'So11111111111111111111111111111111111111112',
-), 'quote_decimals': 9, 'lp_mint': Pubkey.from_string(
-    'Acvzwk3Q6cmEdnVzSfdKLa5786QC44Hmy6cZq5MEZPLc',
-), 'open_orders': Pubkey.from_string(
-    '3pncKJCXwdbd3Hs66T6r9eExiGbSa4fhPVhxzHZNwYFu',
-), 'target_orders': Pubkey.from_string(
-    'HAkvMss1C4rhH4xxZrzJGQSnvj8bNUEE2Q4TrDP6Qnf9',
-), 'base_vault': Pubkey.from_string(
-    'CQqoRGZdH57DBiTyZPjBsycY9EQHin1gHBQFpxzMH8r3',
-), 'quote_vault': Pubkey.from_string(
-    '9ReFnM5HCAr3QUw78mpEASoNZV2xp34Vw9AvCPG7Z1a5',
-), 'market_id': Pubkey.from_string(
-    'pCi9LDihfcukVajicERXEKgD68NVSKKuP1aqGDdenBT',
-), 'market_base_vault': Pubkey.from_string(
-    'A4MY7iRNj3oixzVKLLvHGJyJpHqwJJKPp1Gn5HT725oX',
-), 'market_quote_vault': Pubkey.from_string(
-    'AveWC1Z68nwR9f4fU8BWsZQRZXfNRWuDRLCuf3Acpair',
-), 'market_authority': Pubkey.from_string(
-    '4Pn58WJDp6CSnTGkXbZ4YCKQkLYYC5PTvAMg8xFWXuTK',
-), 'bids': Pubkey.from_string(
-    'BqZNSUrtfwwuzPyd4mRoLXdQCdpLohgJSZgGKdBFDWGH',
-), 'asks': Pubkey.from_string(
-    '6ersikwb7Srqq6fkbx36nmbBruPZXpBmxUYG6jBSyMdc',
-), 'event_queue': Pubkey.from_string(
-    '7CnWTUyDstVfeyWZHonGUvhGmCjhhH4iLEJSTHwV3BQv',
-)}
-
-poolkeysOrange = {'amm_id': Pubkey.from_string(
-    'DVj1swqYnqPzPvhwcGe9JvTJ46szGEVCYhrHZ6oUjia9',
-), 'authority': Pubkey.from_string(
-    '5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1',
-), 'base_mint': Pubkey.from_string(
-    'H5UVM4S9Y5kbuVCEEyJ3NZ38CM8cxUEJrm9gNhswKSdy',
-), 'base_decimals': 9, 'quote_mint': Pubkey.from_string(
-    'So11111111111111111111111111111111111111112',
-), 'quote_decimals': 9, 'lp_mint': Pubkey.from_string(
-    'Acvzwk3Q6cmEdnVzSfdKLa5786QC44Hmy6cZq5MEZPLc',
-), 'open_orders': Pubkey.from_string(
-    '3pncKJCXwdbd3Hs66T6r9eExiGbSa4fhPVhxzHZNwYFu',
-), 'target_orders': Pubkey.from_string(
-    'HAkvMss1C4rhH4xxZrzJGQSnvj8bNUEE2Q4TrDP6Qnf9',
-), 'base_vault': Pubkey.from_string(
-    'CQqoRGZdH57DBiTyZPjBsycY9EQHin1gHBQFpxzMH8r3',
-), 'quote_vault': Pubkey.from_string(
-    '9ReFnM5HCAr3QUw78mpEASoNZV2xp34Vw9AvCPG7Z1a5',
-), 'market_id': Pubkey.from_string(
-    'pCi9LDihfcukVajicERXEKgD68NVSKKuP1aqGDdenBT',
-), 'market_base_vault': Pubkey.from_string(
-    'A4MY7iRNj3oixzVKLLvHGJyJpHqwJJKPp1Gn5HT725oX',
-), 'market_quote_vault': Pubkey.from_string(
-    'AveWC1Z68nwR9f4fU8BWsZQRZXfNRWuDRLCuf3Acpair',
-), 'market_authority': Pubkey.from_string(
-    '4Pn58WJDp6CSnTGkXbZ4YCKQkLYYC5PTvAMg8xFWXuTK',
-), 'bids': Pubkey.from_string(
-    'BqZNSUrtfwwuzPyd4mRoLXdQCdpLohgJSZgGKdBFDWGH',
-), 'asks': Pubkey.from_string(
-    '6ersikwb7Srqq6fkbx36nmbBruPZXpBmxUYG6jBSyMdc',
-), 'event_queue': Pubkey.from_string(
-    '7CnWTUyDstVfeyWZHonGUvhGmCjhhH4iLEJSTHwV3BQv',
-)}
-
+# poolkeys: used for swapping on raydium
 poolkeysBoyoyo = {'amm_id': Pubkey.from_string(
     '6woyrArPvmteXbRMrNwL1hKALZVrvhuQDzPHqmc1xY4Y',
 ), 'authority': Pubkey.from_string(
@@ -130,12 +61,6 @@ def sell_token(TOKEN_TO_SWAP_SELL, amount_to_sell, seed):
         mint = Pubkey.from_string(TOKEN_TO_SWAP_SELL)
         sol = Pubkey.from_string("So11111111111111111111111111111111111111112")
         
-        '''print(payer.pubkey())
-        pool_keys = fetch_pool_keys(str(mint))
-        print(pool_keys)
-        
-        if pool_keys == "failed":
-            return "failed"'''
         
         """Get swap token program id"""
         TOKEN_PROGRAM_ID = solana_client.get_account_info_json_parsed(mint).value.owner
